c86d0bf48ee1a645cf57f8a5026c41017a496b30
Yernar Muratov <yeramuratov@gmail.com>
2025-07-20 03:39:01 +0500

    Auto-generated commit

---
+++ chunk-DEZUBNOJ.js
@@ -0,0 +1,1 @@
+import{ma as t,sa as n,ta as m,va as p}from"./chunk-APJIX734.js";var a=class o{static \u0275fac=function(e){return new(e||o)};static \u0275cmp=t({type:o,selectors:[["app-home"]],decls:2,vars:0,template:function(e,i){e&1&&(n(0,"p"),p(1," home works! "),m())},encapsulation:2})};export{a as HomeComponent};
