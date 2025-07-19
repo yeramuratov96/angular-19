13aff230c600de5a03bad3ec181e88dcc85ff83f
Yernar Muratov <yeramuratov@gmail.com>
2025-07-20 03:38:11 +0500

    chore: добавлены структуры core/shared/features/pages, ESLint, Prettier и деплой на GitHub Pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'home',
+    loadComponent: () => import('./pages/home/home.component').then((m) => m.HomeComponent),
+  },
+];
---
+++ src/app/pages/home/home.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  imports: [],
+  template: `
+    <p>
+      home works!
+    </p>
+  `,
+  styles: ``
+})
+export class HomeComponent {
+
+}
